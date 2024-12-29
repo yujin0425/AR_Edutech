e24ecc683d5771c7ab9ac1aa75ef0d3182437e76
yujin0425 <ich020@naver.com>
2024-12-29 14:52:27 +0900

    section
    안내 서비스 완료(홀로렌즈 연동까지)

--- Hololens_Final_Assignment/Assets/App/Prefabs/UdpSender.cs
+++ Hololens_Final_Assignment/Assets/App/Prefabs/UdpSender.cs
@@ -9,14 +9,13 @@ using TMPro; // TextMeshPro 사용
 public class UdpSender : MonoBehaviour
 {
     private UdpClient udpClient;
-    public string ipAddress = "192.168.254.26"; // Limo 노트북의 IP 주소
+    public string ipAddress = "192.168.0.115"; // Limo 노트북의 IP 주소
     public int port = 2020; // Ubuntu에서 수신할 포트 번호
     public PressableButtonHoloLens2 sendButton; // MRTK의 PressableButtonHoloLens2 사용
-    public int inputType; //1: UI, 2: Gesutre
+    public int inputType; //1: UI, 2: Gesture
 
     public int gestureNUM;
 
-    
     private TextMeshPro buttonText;
 
     void Start()
@@ -25,146 +24,54 @@ public class UdpSender : MonoBehaviour
         // UdpClient 초기화
         udpClient = new UdpClient();
 
-        // 버튼 클릭 시 OnSendData 호출
-        //sendButton.OnButtonPressed.AddListener(OnSendData);
-
         // 버튼 내의 TextMeshPro 컴포넌트를 찾아서 참조
         buttonText = sendButton.GetComponentInChildren<TextMeshPro>();
     }
+
     void Update()
     {
         Debug.Log("NUM:" + gestureNUM);
-        if(inputType == 2)
+        if (inputType == 2)
         {
-            if(gestureNUM == 1)
+            if (gestureNUM == 1)
             {
-                try
-                {
-                    string message = "GO"; // 보낼 메시지
-                    byte[] data = Encoding.UTF8.GetBytes(message); // 메시지를 바이트 배열로 변환
-
-                    // IP 주소 및 포트로 데이터 전송
-                    udpClient.Send(data, data.Length, ipAddress, port);
-                    Debug.Log("Data sent: " + message);
-                }
-                catch (Exception e)
-                {
-                    Debug.LogError("Error sending UDP data: " + e.Message);
-                }
+                SendUdpMessage("GO");
             }
-            else if(gestureNUM == 2)
+            else if (gestureNUM == 2)
             {
-                try
-                {
-                    string message = "STOP"; // 보낼 메시지
-                    byte[] data = Encoding.UTF8.GetBytes(message); // 메시지를 바이트 배열로 변환
-
-                    // IP 주소 및 포트로 데이터 전송
-                    udpClient.Send(data, data.Length, ipAddress, port);
-                    Debug.Log("Data sent: " + message);
-                }
-                catch (Exception e)
-                {
-                    Debug.LogError("Error sending UDP data: " + e.Message);
-                }
+                SendUdpMessage("STOP");
             }
-            else if(gestureNUM == 3)
+            else if (gestureNUM == 3)
             {
-                try
-                {
-                    string message = "ROTATE"; // 보낼 메시지
-                    byte[] data = Encoding.UTF8.GetBytes(message); // 메시지를 바이트 배열로 변환
-
-                    // IP 주소 및 포트로 데이터 전송
-                    udpClient.Send(data, data.Length, ipAddress, port);
-                    Debug.Log("Data sent: " + message);
-                }
-                catch (Exception e)
-                {
-                    Debug.LogError("Error sending UDP data: " + e.Message);
-                }
+                SendUdpMessage("ROTATE");
             }
         }
     }
+
     public void OnSendData_Up()
     {
         if (inputType == 1)
         {
-            try
-            {
-                string message = "Up"; // 보낼 메시지
-                byte[] data = Encoding.UTF8.GetBytes(message); // 메시지를 바이트 배열로 변환
-
-                // IP 주소 및 포트로 데이터 전송
-                udpClient.Send(data, data.Length, ipAddress, port);
-                Debug.Log("Data sent: " + message);
-            }
-            catch (Exception e)
-            {
-                Debug.LogError("Error sending UDP data: " + e.Message);
-            }
+            SendUdpMessage("section1"); // 로봇이 section1로 이동
         }
-
     }
+
     public void OnSendData_Down()
     {
         if (inputType == 1)
         {
-            try
-            {
-                string message = "Down"; // 보낼 메시지
-                byte[] data = Encoding.UTF8.GetBytes(message); // 메시지를 바이트 배열로 변환
-
-                // IP 주소 및 포트로 데이터 전송
-                udpClient.Send(data, data.Length, ipAddress, port);
-                Debug.Log("Data sent: " + message);
-            }
-            catch (Exception e)
-            {
-                Debug.LogError("Error sending UDP data: " + e.Message);
-            }
+            SendUdpMessage("section3"); // 로봇이 section3로 이동
         }
-
     }
-    public void OnSendData_Left()
-    {
-        if (inputType == 1)
-        {
-            try
-            {
-                string message = "Left"; // 보낼 메시지
-                byte[] data = Encoding.UTF8.GetBytes(message); // 메시지를 바이트 배열로 변환
 
-                // IP 주소 및 포트로 데이터 전송
-                udpClient.Send(data, data.Length, ipAddress, port);
-                Debug.Log("Data sent: " + message);
-            }
-            catch (Exception e)
-            {
-                Debug.LogError("Error sending UDP data: " + e.Message);
-            }
-        }
-
-    }
     public void OnSendData_Right()
     {
         if (inputType == 1)
         {
-            try
-            {
-                string message = "Right"; // 보낼 메시지
-                byte[] data = Encoding.UTF8.GetBytes(message); // 메시지를 바이트 배열로 변환
-
-                // IP 주소 및 포트로 데이터 전송
-                udpClient.Send(data, data.Length, ipAddress, port);
-                Debug.Log("Data sent: " + message);
-            }
-            catch (Exception e)
-            {
-                Debug.LogError("Error sending UDP data: " + e.Message);
-            }
+            SendUdpMessage("section2"); // 로봇이 section2로 이동
         }
     }
+
     public void OnConvertInput()
     {
         if (inputType == 1)
@@ -179,11 +86,23 @@ public class UdpSender : MonoBehaviour
             Debug.Log("1");
             buttonText.text = "UI";
         }
-        
-        
-
     }
 
+    private void SendUdpMessage(string message)
+    {
+        try
+        {
+            byte[] data = Encoding.UTF8.GetBytes(message); // 메시지를 바이트 배열로 변환
+
+            // IP 주소 및 포트로 데이터 전송
+            udpClient.Send(data, data.Length, ipAddress, port);
+            Debug.Log("Data sent: " + message);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Error sending UDP data: " + e.Message);
+        }
+    }
 
     private void OnApplicationQuit()
     {
@@ -193,6 +112,4 @@ public class UdpSender : MonoBehaviour
             udpClient.Close();
         }
     }
-
-   
 }
